ae1de515743c6ba0eb2d95ff5f767f6a9bda9d4e
Kinsa Durst <kinsadurst@gmail.com>
2018-02-18 19:02:19 -0800

    add publish and subscribe in publisher.py

--- publisher.py
+++ publisher.py
@@ -1,5 +1,7 @@
 from pubnub.pnconfiguration import PNConfiguration
-from pubnub.pubnub import PubNub
+from pubnub.pubnub import PubNub, SubscribeListener
+from pubnub.exceptions import PubNubException
+from pubnub.enums import PNStatusCategory
 
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = 'sub-c-ce1f7efa-0bda-11e8-8ffb-b29a975517c3'
@@ -8,4 +10,57 @@ pnconfig.ssl = True
 
 #initialize pubnub with configuration settings
 pubnub = PubNub(pnconfig)
+
+my_listener = SubscribeListener()
+pubnub.add_listener(my_listener)
+
+pubnub.subscribe().channels('aqi').execute()
+my_listener.wait_for_connect()
+print('connected')
+
+pubnub.publish().channel('aqi').message({
+    'aqi': 'over 9000',
+    'co2': 21
+}).sync()
+
+result = my_listener.wait_for_message_on('aqi')
+print(result.message)
+print("end of message")
+
+
+
+
+
+
+
+
+
+# class MyListener(SubscribeCallback):
+#     def status(self, pubnub, status):
+#         if status.category == PNStatusCategory.PNDisconnectedCategory:
+#             pubnub.publish().channel('aqi').message({'aqi': 'over 9000', 'co2': 21 }).sync()
+    
+#     def message(self, pubnub, message):
+#         pass
+    
+#     def presence(self, pubnub, presence):
+#         pass
+
+# my_istener = MyListener()
+
+# pubnub.add_listener(my_istener)
+
+# pubnub.subscribe().channels('aqi').execute()
+
+
+# pubnub.subscribe().channels('my_channel').execute()
+
+# try:
+#     envelope = pubnub.publish().channel('my_channel').message({
+#         'name': 'Kinsa',
+#         'online': True
+#     }).sync()
+#     print('publish timetoken: %d' % envelope.result.timetoken)
+# except PubNubException as e:
+#     print(e)
 
