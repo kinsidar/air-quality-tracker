4f59360b677dc4f78a908f1dd24de31f2fbe03be
kinsidar <kinsadurst@gmail.com>
2018-03-05 14:26:40 -0800

    add presence feature, publisher prints occupancy and uuid of subscribers

--- publisher.py
+++ publisher.py
@@ -2,7 +2,7 @@ from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from pubnub.pubnub import SubscribeListener
 from pubnub.exceptions import PubNubException
-from pubnub.enums import PNStatusCategory
+from pubnub.enums import PNStatusCategory, PNOperationType
 import threading
 import urllib, json, sys
 
@@ -12,39 +12,52 @@ lon = "-122.1"
 aqicn_api_token = "214e3324769450fc0bc5688dac030affbc4d48a1"
 url = "https://api.waqi.info/feed/geo:" + lat + ";" + lon + "/?token=" + aqicn_api_token
 
-#pubnub configuration settings
+#initialize pubnub
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = 'sub-c-ce1f7efa-0bda-11e8-8ffb-b29a975517c3'
 pnconfig.publish_key = 'pub-c-b43f4454-c2c0-4587-aa51-a6785db8406f'
 pnconfig.ssl = True
+
 pubnub = PubNub(pnconfig)
 
 def main():
-    # pubnub = PubNub(pnconfig)
-    my_listener = SubscribeListener()
-    pubnub.add_listener(my_listener)
-    pubnub.subscribe().channels('aqi').execute()
-    my_listener.wait_for_connect()
-    print('connected')  
-
     loop()
+
+def here_now_callback(result, status):
+    if status.is_error():
+        print("here now callback error")
+        return
+    for channel_data in result.channels:
+        print("---")
+        print("channel: %s" % channel_data.channel_name)
+        print("occupancy: %s" %channel_data.occupancy)
+    for occupant in channel_data.occupants:
+        print("uuid: %s, state: %s" % (occupant.uuid, occupant.state))
 
 def loop():
     try:
         response = urllib.request.urlopen(url)
     except:
-        print("URL Error")
-        sys.exit(0)
+        sys.exit("url error")
 
     threading.Timer(10, loop).start()
 
     #publish aqi
     data = json.loads(response.read())
-    pubnub.publish().channel('aqi').message({
-        'aqi': data['data']['aqi'],
-        'city': data['data']['city']['name'],
-        'time': data['data']['time']['s']
-    }).sync()
-  
+    try:
+        pubnub.publish().channel('aqi').message({
+            'aqi': data['data']['aqi'],
+            'city': data['data']['city']['name'],
+            'time': data['data']['time']['s']
+        }).sync()
+    except PubNubException as e:
+        handle_exception(e)
+
+    #detect presence
+    pubnub.here_now()\
+    .channels('aqi')\
+    .include_uuids(True)\
+    .async(here_now_callback)
+
 if __name__ == "__main__":
    main()
